e608f7dccb02be7abdaa1b5107e99445dccc5420
maxiamikel19 <maxloversist19@gmail.com>
2024-05-03 15:34:25 -0300

    Initian config

---
+++ api-online-shop/src/test/java/com/maxi/apionlineshop/ApiOnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxi.apionlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
